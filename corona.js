a372983d0cce24923d85b1756893fef022c46f00
shirin <shirinanlen@gmail.com>
2020-04-30 13:57:56 -0400

    working on dna

--- corona.js
+++ corona.js
@@ -1,37 +1,37 @@
 class Corona {
-  constructor(){
+  constructor() {
     this.shapes = []
   }
 
-  render(){
-  let picker = random(1)
-  if(picker > 0.3) {
-    this.shapes.push(new Circles())
-  }
-  picker = random(1)
-  if(picker > 0.3) {
-    this.shapes.push(new DottedLines())
-  }
-  picker = random(1)
-  if(picker > 0.3) {
-    this.shapes.push(new CenteredShape())
-  }
-  this.shapes.forEach(shape => {
-    shape.render()
-  })
+  render() {
+    let picker = random(1)
+    if (picker > 0.3) {
+      this.shapes.push(new Circles())
+    }
+    picker = random(1)
+    if (picker > 0.3) {
+      this.shapes.push(new DottedLines())
+    }
+    picker = random(1)
+    if (picker > 0.3) {
+      this.shapes.push(new CenteredShape())
+    }
+    this.shapes.forEach(shape => {
+      shape.render()
+    })
   }
 }
 
 function randomSelectTwo() {
   const rando = random(1);
   if (rando > 0.5) {
-   return true;
+    return true;
   } else {
-   return false; 
+    return false;
   }
- }
- 
- function randomColorPallete(){
-  const randoColor = floor(random(0 ,PALLETE.length));
+}
+
+function randomColorPallete() {
+  const randoColor = floor(random(0, PALLETE.length));
   return PALLETE[randoColor];
- }
+}
--- sketch.js
+++ sketch.js
@@ -1,8 +1,14 @@
-const CRYSTAL_SIZE = 150;
+const CRYSTAL_SIZE = 25;
 const SIDES = 6;
 let PALLETE = [];
 let corona;
 let rocket;
+let population;
+const lifeSpan = 200;
+let lifeP;
+let count = 0;
+let target;
+
 
 function setup() {
   // noLoop()
@@ -12,40 +18,137 @@ function setup() {
     color(4, 0, 152) //blue
   ]
   angleMode(DEGREES);
+
   rocket = new Rocket();
+  population = new Population();
+  lifeP = createP();
+  target = createVector(width / 2, 50);
 }
 
 function draw() {
   background(0)
-  corona = new Corona()
-  rocket.update();
-  rocket.show();
-  
-  
-  
+  corona = new Corona();
+  population.run();
+  lifeP.html(count);
+  count++;
+
+  if (count == lifeSpan) {
+    count = 0;
+    //reset
+    population = new Population();
+  }
+
+  ellipse(target.x, target.y, 16, 16);
+
+}
+
+function Population() {
+  this.rockets = [];
+  this.popsize = 25;
+  this.matingpool = [];
+
+  for (let i = 0; i < this.popsize; i++) {
+    this.rockets[i] = new Rocket();
+  }
+
+  this.evaluate = function () {
+    let maxFit = 0;
+    for (let i = 0; i < this.popsize; i++) {
+      this.rockets[i].calcFitness();
+      if (this.rokects[i].fitness > maxFit) {
+        maxFit = this.rokects[i].fitness;
+      }
+    }
+    for (let i = 0; i < this.popsize; i++) {
+      this.rockets[i].fitness /= maxFit;
+    }
+
+    this.matingpool = [];
+    for (let i = 0; i < this.popsize; i++) {
+      let n = this.rockets[i].fitness * 100;
+      for (let j = 0; j < n; j++) {
+        this.matingpool.add(this.rockets[i]);
+      }
+    }
+  }
+
+  this.selection = function () {
+    let parentA = random(this.matingpool).dna;
+    let parentB = random(this.matingpool).dna;
+    let child = parentA.crossover(parentB);
+  }
+
+
+  this.run = function () {
+    for (let i = 0; i < this.popsize; i++) {
+      this.rockets[i].update();
+      this.rockets[i].show();
+
+    }
+  }
 }
 
 
-function Rocket() {
-  this.pos = createVector(width/2, height);
-  this.vel = p5.Vector.random2D();
-  this.acc = createVector();
+function DNA(genes) {
+  if(genes) {
+    this.genes = genes;
+  } else {
+    this.genes = [];
+    for (let i = 0; i < lifeSpan; i++) {
+      this.genes[i] = p5.Vector.random2D();
+      this.genes[i].setMag(0.3); //speed
+  }
+}
 
-  this.applyForce = function(force) {
+  this.crossover = function (partner) {
+    let newgenes = [];
+    let mid = floor(random(genes.length));
+    for (let i = 0; i < genes.length; i++) {
+      if (i > mid) {
+        newgenes[i] = this.genes[i];
+      } else {
+        newgenes[i] = this.partner[i];
+      }
+    }
+    return new DNA(newgenes);
+  }
+
+}
+
+
+
+
+
+
+
+function Rocket() {
+  this.pos = createVector(width / 2, height);
+  this.vel = createVector();
+  this.acc = createVector();
+  this.dna = new DNA();
+  this.fitness = 0;
+
+  this.applyForce = function (force) {
     this.acc.add(force);
   }
 
-  this.update = function() {
+  this.calcFitness = function () {
+    let d = dist(this.pos.x, this.pos.y, target.x, target.y);
+    this.fitness = map(d, 0, width, width, 0);
+  }
+
+  this.update = function () {
+    this.applyForce(this.dna.genes[count]);
+
     this.vel.add(this.acc);
     this.pos.add(this.vel);
     this.acc.mult(0);
   }
 
-  this.show = function() {
+
+  this.show = function () {
     push();
-    // noStroke();
-    // fill(255, 150);
-    translate(this.pos.x , this.pos.y);
+    translate(this.pos.x, this.pos.y);
     rotate(this.vel.heading());
     corona.render()
     // rectMode(CENTER);
